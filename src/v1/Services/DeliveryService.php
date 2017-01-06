f74326b358cc8386d3cedcd418768d43bcd39aad
Luciano Porto <luciano.bapo@gmail.com>
2017-01-06 00:57:44 -0200

    automatic-commit-alias

--- src/v1/Services/DeliveryService.php
+++ src/v1/Services/DeliveryService.php
@@ -9,29 +9,32 @@
 
 namespace ErpNET\Delivery\v1\Services;
 
+use ErpNET\Models\v1\Criteria\ProductGroupActivatedCriteria;
+use ErpNET\Models\v1\Criteria\ProductGroupCategoriesCriteria;
 use ErpNET\Models\v1\Interfaces\ProductRepository;
 use ErpNET\Delivery\v1\Entities\DeliveryPackageEloquent;
-use ErpNET\Models\v1\Interfaces\SharedStatRepository;
+use ErpNET\Models\v1\Interfaces\ProductGroupRepository;
 
 class DeliveryService
 {
     protected $productRepository;
+    protected $productGroupRepository;
     /**
      * Service constructor.
      */
-    public function __construct(ProductRepository $productRepository, SharedStatRepository $sharedStatRepository)
+    public function __construct(ProductRepository $productRepository, ProductGroupRepository $productGroupRepository)
     {
         $this->productRepository = $productRepository;
-        $this->sharedStatRepository = $sharedStatRepository;
+        $this->productGroupRepository = $productGroupRepository;
     }
 
     public function deliveryPackage()
     {
-//        $aa = new DeliveryPackageEloquent;
-//        dd($aa);
-        $sharedStats = $this->sharedStatRepository->pushCriteria();
-        $sharedStats = $this->sharedStatRepository->all();
-        dd($sharedStats);
+        $productGroups = $this->productGroupRepository
+            ->pushCriteria(ProductGroupCategoriesCriteria::class)
+            ->pushCriteria(ProductGroupActivatedCriteria::class)
+            ->all();
+        dd($productGroups);
         $products = $this->productRepository->all();
         
         return $products;
